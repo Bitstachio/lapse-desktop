2f14a4b5c1064e250364bbac4fd5f049bf0dde33
Barbod H. <ph.barbod@gmail.com>
2025-08-06 14:55:55 -0400

    Set up Bootstrap & Bootswatch

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,11 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import 'bootswatch/dist/flatly/bootstrap.min.css';
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App.tsx";
 import "./index.css";
-import { BrowserRouter } from "react-router-dom";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const queryClient = new QueryClient();
 
