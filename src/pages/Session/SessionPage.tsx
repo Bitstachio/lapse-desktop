20b8ecf50db11364373119377cf62cffafb6133c
Barbod H. <ph.barbod@gmail.com>
2025-08-13 15:54:59 -0400

    Polish SessionPage

--- src/pages/Session/SessionPage.tsx
+++ src/pages/Session/SessionPage.tsx
@@ -1,5 +1,4 @@
 import { useEffect, useState } from "react";
-import socket from "../../api/socket.ts";
 import Countdown from "../../components/Countdown/Countdown.tsx";
 import CountdownController from "../../components/CountdownController/CountdownController.tsx";
 import TimeoutModal from "../../components/TimeoutModal/TimeoutModal.tsx";
@@ -7,44 +6,20 @@ import useProcessManager from "../../hooks/useProcessManager.ts";
 
 const DashboardPage = () => {
   const [remainingDuration, setRemainingDuration] = useState<number | undefined>();
-  const [isTimeout, setIsTimeout] = useState(false);
 
-  const [message, setMessage] = useState("");
-
-  const processManager = useProcessManager("BX50", 4);
+  const { state, status, start, pause, resume, extend, finish } = useProcessManager("BX50", 4);
 
   useEffect(() => {
-    socket.on("process-timeout", (data) => {
-      setMessage(data.type);
-      if (data.type === "PROCESS_TIMEOUT") {
-        setIsTimeout(true);
-      } else {
-        // data.type === "EXTENSION_TIMEOUT"
-      }
-    });
-
-    return () => {
-      socket.off("process-timeout");
-    };
-  }, []);
-
-  useEffect(() => {
-    if (processManager.status.isSuccess && !processManager.status.isFetching && processManager.status.data) {
-      setRemainingDuration(processManager.status.data.interval.remainingDuration);
+    if (status.isSuccess && !status.isFetching && status.data) {
+      setRemainingDuration(status.data.interval.remainingDuration);
     }
-  }, [processManager.status]);
+  }, [status]);
 
   return (
     <main>
-      <p>Message: {message}</p>
-      {isTimeout && <TimeoutModal onExtend={processManager.extend} onFinish={processManager.finish} />}
-      <Countdown processState={processManager.state} startingDuration={remainingDuration} />
-      <CountdownController
-        state={processManager.state}
-        onStart={processManager.start}
-        onPause={processManager.pause}
-        onResume={processManager.resume}
-      />
+      {state === "timeout" && <TimeoutModal onExtend={extend} onFinish={finish} />}
+      <Countdown processState={state} startingDuration={remainingDuration} />
+      <CountdownController state={state} onStart={start} onPause={pause} onResume={resume} />
     </main>
   );
 };
