b044c0590a6067c8f3e61d8bc71af842cf7e48bd
Barbod H. <ph.barbod@gmail.com>
2025-08-20 15:40:38 -0400

    Test commit

--- src/pages/Session/SessionPage.tsx
+++ src/pages/Session/SessionPage.tsx
@@ -5,24 +5,25 @@ import TimeoutModal from "../../features/countdown/components/TimeoutModal/Timeo
 import useProcessManager from "../../features/process/hooks/useProcessManager.ts";
 
 const SessionPage = () => {
-  const [remainingDuration, setRemainingDuration] = useState<number | undefined>();
+  // const [remainingDuration, setRemainingDuration] = useState<number | undefined>();
 
-  const { state, status, start, pause, resume, extend, finish } = useProcessManager("BX50", 4);
+  // const { state, status, start, pause, resume, extend, finish } = useProcessManager("BX50", 4);
 
-  useEffect(() => {
-    if (status.isSuccess && !status.isFetching && status.data) {
-      setRemainingDuration(status.data.interval.remainingDuration);
-    }
-  }, [status]);
+  // useEffect(() => {
+  //   if (status.isSuccess && !status.isFetching && status.data) {
+  //     setRemainingDuration(status.data.interval.remainingDuration);
+  //   }
+  // }, [status]);
 
   return (
     <main>
-      <Countdown processState={state} startingDuration={remainingDuration} />
+      <h1>Hello</h1>
+      {/* <Countdown processState={state} startingDuration={remainingDuration} />
       {state === "timeout" ? (
         <TimeoutModal onExtend={extend} onFinish={finish} />
       ) : (
         <CountdownController state={state} onStart={start} onPause={pause} onResume={resume} />
-      )}
+      )} */}
     </main>
   );
 };
